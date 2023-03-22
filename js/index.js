d9f741699413821df709b17e51b6bb78bd27c4ff
Angel Madin <madinangel05@gmail.com>
2023-03-22 16:21:53 +0200

    working

--- js/index.js
+++ js/index.js
@@ -1,13 +1,16 @@
 function onScrollHandler() {
     let arrowEl = document.querySelector('.arrow-up')
+    let phoneIconEl = document.querySelector('.connect')
 
-    arrowEl.addEventListener('click', function() {
+    // arrowEl.addEventListener('click', function() {
         
-    })
+    // })
 
     if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
         arrowEl.style.display = 'block';
+        phoneIconEl.style.display = 'block';
     } else {
         arrowEl.style.display = 'none';
+        phoneIconEl.style.display = 'none';
     }
 }
